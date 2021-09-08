b6c22cfef831b734dbd2484f177e84567724e1db
robin <robinkaris7@gmail.com>
2021-09-08 20:28:33 +0300

    made changes to newsItem

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 
 
 
--- src/components/newsItem.js
+++ src/components/newsItem.js
@@ -11,16 +11,18 @@ export const NewsItem= (props) => {
       <Col  xs={12} sm={4} md={3}>
         <div key={url.toString()} >
             <Card className='mt-3'>
+             
   <Card.Header>{props.props.byline}</Card.Header>
   <Card.Body>
     
     <Image src={url["media-metadata"][2].url} thumbnail ></Image>
     <Card.Title>{props.props.title}</Card.Title>
+    <hr></hr>
     <Card.Text>
       {props.props.abstract}
     </Card.Text>
-    
-    <Button href={props.props.url} variant="primary">Read More</Button>
+    <hr></hr>
+    <Button href={props.props.url} variant="dark">Read More</Button>
     <br></br>
     {props.props.published_date}
   </Card.Body>
