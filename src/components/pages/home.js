4d433255d099c02f36e3ef5f2d7901ca67117c33
robin <robinkaris7@gmail.com>
2021-09-08 21:24:59 +0300

    made chnages to home

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -6,28 +6,37 @@ import axios from "axios";
 
 
 
+
 export const Home = () => {
     const [news,setNews] = useState([])
+    const [isLoading,setIsLoading] = useState(true)
     const getNewsList =()=>{
         const url = `https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=${process.env.REACT_APP_API_KEY}` 
         axios.get(url)
         .then((response)=>{
              setNews(response.data.results)
+             setIsLoading(false)
+
             
         })
     }
     useEffect(() => {  
     getNewsList()
     },[])
-
+  
 
     return (
+        
 
         <div>
            <NavBar/>
-           
-           <NewsList props={news}/>
+         {
+             (isLoading === true)?<h1 style={{textAlign:"center"}}>No Items</h1>:<NewsList props={news} />
+
+         }
+      
             
         </div>
     )
 }
+
